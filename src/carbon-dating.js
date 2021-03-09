89b57a5ac611e976310de5c8e0906222a9db49b2
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-03-09 23:22:33 +0300

    feat: update solution for other test's

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(current) {
-  if (typeof(current) === 'string' && 0 < current <= MODERN_ACTIVITY) {
-    return Math.ln(current/MODERN_ACTIVITY) / (0.693/HALF_LIFE_PERIOD)
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'string' && !!sampleActivity.trim() 
+    && 0 < sampleActivity && sampleActivity <= MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)) / (0.693/HALF_LIFE_PERIOD))
   } 
   return false;
 };
