8b6ec1e48983761cfa35ed06f7bf942ae549a79a
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-03-10 01:29:03 +0300

    feat: add temp-solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,17 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    /*
+    'repeatTimes' in options 
+    'separator' in options
+    'addition' in options 
+    'additionRepeatTimes' in options
+    'additionSeparator' in options
+    */
+    for (let i=0; i<options.repeatTimes; i++){
+        for (let j=0; j<options.additionRepeatTimes; j++){
+            'addition' in options && (str += options.addition);
+            'additionSeparator' in options && (str += options.addition);
+        }
+        'separator' in options ? str += options.separator : str += '+';
+    }
+    return str;
 };
-  
