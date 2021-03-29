0350c74a3848c38ab02f86d32975133c8d4983f6
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-03-30 01:30:47 +0300

    feat: add term solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,22 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if(!Array.isArray(arr)) throw new Error();
+
+    let res = [];
+    let j = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === '--double-next'){
+            i < arr.length-1 ? res[j] = arr[i+1] : res[j] = undefined;
+        } else if(arr[i] === '--double-prev'){
+            i > 0 ? res[j] = arr[i-1] : res[j] = undefined;
+        } else if(arr[i] === '--discard-prev'){
+            i++;
+            i > -1 ? res[j-1] = arr[i] : res[j] = undefined;
+        } else if(arr[i] === '--discard-next'){
+            i += 2;
+            i < arr.length-1 ? res[j] = arr[i] : res[j] = undefined;
+        } else res[j] = arr[i];
+        j++;
+    }
+    return res.filter(e => e !== undefined);
 };
